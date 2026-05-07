526ec385fc593d7db239cea5348c1e43df2d4bdd
Ranjan-navneet <mail.navneetranjan@gmail.com>
2026-05-07 22:01:58 +0530

    added server and env chnges

--- server.js
+++ server.js
@@ -12,18 +12,12 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 const allowedOrigins = [
   "http://localhost:3000",
-  "https://lovableps1308.netlify.app/",
+  "https://lovableps1308.netlify.app",
 ];
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: allowedOrigins,
     credentials: true,
   }),
 );
