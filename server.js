5003a38904638b6ce582e0c90ecab0d327aefb32
Ranjan-navneet <mail.navneetranjan@gmail.com>
2026-05-07 21:51:21 +0530

    added server and env chnges

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 const allowedOrigins = [
   "http://localhost:3000",
-  "https://69fb9407eff0532a8dcca3e5--lovable1308.netlify.app",
+  "https://lovableps1308.netlify.app/",
 ];
 
 app.use(
@@ -41,7 +41,7 @@ app.get("/api/health", (req, res) => {
 
 // MongoDB connection
 mongoose
-  .connect(process.env.MONGO_URI || "mongodb+srv://Navneet:navneet123@navneet.ehuztto.mongodb.net/loveable?retryWrites=true&w=majority")
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("✅ MongoDB connected");
     app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
